323de65beeb0d53ef6e275997eb2bd2aa67988b4
ASTIK <astik.ytt@gmail.com>
2024-01-02 00:35:11 +0100

    Add 'js' task

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
 
 
 
@@ -26,11 +27,12 @@ function watcher() {
 	gulp.watch(path.watch.files, copy);
 	gulp.watch(path.watch.html, html);
 	gulp.watch(path.watch.scss, scss);
+	gulp.watch(path.watch.js, js);
 }
 
 
 
-const mainTasks = gulp.parallel(copy, html, scss);
+const mainTasks = gulp.parallel(copy, html, scss, js);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 

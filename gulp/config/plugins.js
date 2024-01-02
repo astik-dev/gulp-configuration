4b74fa16b27b2182b4eb5ba836c344d2ab16183f
ASTIK <astik.ytt@gmail.com>
2024-01-03 00:24:05 +0100

    Add 'images' task

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,7 +1,9 @@
 import plumber from "gulp-plumber";
 import browsersync from "browser-sync";
+import newer from "gulp-newer";
 
 export const plugins = {
 	plumber: plumber,
 	browsersync: browsersync,
+	newer: newer,
 }
---
+++ gulp/tasks/images.js
@@ -0,0 +1,26 @@
+import webp from "gulp-webp";
+import imagemin, {gifsicle, mozjpeg} from "gulp-imagemin";
+import pngquant from 'imagemin-pngquant';
+
+export const images = () => {
+	return app.gulp.src(app.path.src.images)
+		.pipe(app.plugins.plumber())
+		.pipe(app.plugins.newer(app.path.build.images))
+
+		.pipe(webp({quality: 70}))
+		.pipe(app.gulp.dest(app.path.build.images))
+
+		.pipe(app.gulp.src(app.path.src.images))
+		.pipe(app.plugins.newer(app.path.build.images))
+		.pipe(imagemin([
+			gifsicle({interlaced: true}),
+			mozjpeg({quality: 80, progressive: true}),
+			pngquant({quality: [0.8, 0.8]}),
+		]))
+		.pipe(app.gulp.dest(app.path.build.images))
+
+		.pipe(app.gulp.src(app.path.src.svg))
+		.pipe(app.gulp.dest(app.path.build.images))
+
+		.pipe(app.plugins.browsersync.stream());
+}

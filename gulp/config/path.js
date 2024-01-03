5f821a1891b31feb337e0909d081eb9d420f208b
ASTIK <astik.ytt@gmail.com>
2024-01-03 22:46:38 +0100

    Add 'svgsprite' task

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -20,6 +20,7 @@ export const path = {
 		scss: `${srcFolder}/scss/style.scss`,
 		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
+		svgicons: `${srcFolder}/svgicons/*.svg`,
 	},
 	watch: {
 		js: `${srcFolder}/js/**/*.js`,
@@ -27,6 +28,7 @@ export const path = {
 		html: `${srcFolder}/**/*.html`,
 		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
 		files: `${srcFolder}/files/**/*.*`,
+		svgicons: `${srcFolder}/svgicons/*.svg`,
 	},
 	buildFolder: buildFolder,
 	srcFolder: srcFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
+import { svgsprite } from "./gulp/tasks/svgsprite.js";
 
 
 
@@ -30,11 +31,12 @@ function watcher() {
 	gulp.watch(path.watch.scss, scss);
 	gulp.watch(path.watch.js, js);
 	gulp.watch(path.watch.images, images);
+	gulp.watch(path.watch.svgicons, svgsprite);
 }
 
 
 
-const mainTasks = gulp.parallel(copy, html, scss, js, images);
+const mainTasks = gulp.parallel(copy, html, scss, js, images, svgsprite);
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 

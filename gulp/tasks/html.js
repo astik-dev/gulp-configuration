c551f82cc4405eedf486c747157421485f248c67
ASTIK <astik.ytt@gmail.com>
2024-01-13 00:07:36 +0100

    Add 'gulp-webp-html-nosvg' plugin for 'html' task

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,16 @@
 import fileInclude from "gulp-file-include";
 import versionNumber from "gulp-version-number";
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
 		.pipe(app.plugins.plumber())
 		.pipe(fileInclude())
+		.pipe(
+			app.plugins.if(app.isBuild,
+				webpHtmlNosvg()
+			)
+		)
 		.pipe(
 			app.plugins.if(app.isBuild,
 				versionNumber({
